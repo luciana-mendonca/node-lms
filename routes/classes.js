c65409115de23669943085e862f4f407833c73d3
Luci-Lawless <lu.gmmm@gmail.com>
2018-11-16 21:14:57 +0100

    Create courses index route, render course' single page

--- routes/classes.js
+++ routes/classes.js
@@ -2,11 +2,25 @@ var express = require('express');
 var router = express.Router();
 var Class = require('../models/class');
 
-/* GET home page. */
+// All courses
 router.get('/', function(req, res, next) {
   Class.getClasses(function(err, classes) {
+    if(err) {
+      throw err;
+    }
     res.render('classes/index', { title: 'All Courses', classes: classes });
   }, 4);
+});
+
+// Single course
+router.get('/:id/details', function(req, res, next) {
+  var id = req.params.id;
+  Class.getClassById(id, function(err, classname) {
+    if(err) {
+      throw err;
+    }
+    res.render('classes/details', {class: classname });
+  });
 });
 
 module.exports = router;
