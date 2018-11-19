c0440cd9cc5ae33eabdee83e4cb53dc7475c299f
Luci-Lawless <lu.gmmm@gmail.com>
2018-11-19 11:23:14 +0100

    Encrypt users password

--- models/user.js
+++ models/user.js
@@ -28,3 +28,39 @@ module.exports.getUserByUsername = function(username, cb) {
   var query = {username: username};
   User.findOne(query, cb);
 };
+
+// Compare passwords
+module.exports.comparePassword = function(userPassword, hash, cb) {
+  bcrypt.compare(userPassword, hash, function(err, isMatch) {
+    if(err) {
+      throw err;
+    }
+    cb(null, isMatch);
+  });
+}
+
+// Create user type student and hash password with bcrypt
+module.exports.saveStudent = function(newUser, newStudent, cb) {
+  bcrypt.hash(newUser.password, 10, function(err, hash) {
+    if(err) {
+      throw err;
+    }
+    // Hash password
+    newUser.password = hash;
+    // Save user in two different collections
+    async.parallel([newUser.save, newStudent.save], cb);
+  });
+}
+
+// Create user type instructor and hash password with bcrypt
+module.exports.saveStudent = function(newUser, newInstructor, cb) {
+  bcrypt.hash(newUser.password, 10, function(err, hash) {
+    if(err) {
+      throw err;
+    }
+    // Hash password
+    newUser.password = hash;
+    // Save user in two different collections
+    async.parallel([newUser.save, newInstructor.save], cb);
+  });
+}
